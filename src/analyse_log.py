8987f5cea6aa86c042a71f391a554b80c5a9d439
dougfunny <douglaseduardo220483@gmail.com>
2020-09-17 19:33:35 -0300

    começando oprojeto restaurant orders

--- src/analyse_log.py
+++ src/analyse_log.py
@@ -1,2 +1,102 @@
-def analyse_log(path_to_file):
-    raise NotImplementedError
+from csv import DictReader
+
+
+def most_frequent_maria(client):
+    most_frequent_lunch = client[0]["lunch"]
+    frequency = {}
+    for lunch in client:
+        if lunch["lunch"] not in frequency:
+            frequency[lunch["lunch"]] = 1
+        else:
+            frequency[lunch["lunch"]] += 1
+
+        if frequency[lunch["lunch"]] > frequency[most_frequent_lunch]:
+            most_frequent_lunch = lunch["lunch"]
+    return most_frequent_lunch
+
+
+def f_frequency(keys, client, params_map_key):
+    frequency = {k: 0 for k in keys}
+    for all_days in client:
+        if all_days[params_map_key] not in frequency:
+            frequency[all_days[params_map_key]] = 1
+        else:
+            frequency[all_days[params_map_key]] += 1
+    return frequency
+
+
+def cont_frequent_arnaldo_lunch(keys, client, params_map_key):
+    frequency = f_frequency(keys, client, params_map_key)
+    answer = ""
+    for k, v in frequency.items():
+        if k == "hamburguer":
+            answer += f"foram {v} {k}, "
+    return answer
+
+
+def cont_frequent_joao(keys, client, params_map_key):
+    frequency = f_frequency(keys, client, params_map_key)
+    answer = " "
+    for k, v in frequency.items():
+        if v == 0:
+            answer += f"{k}, "
+    return answer
+
+
+def txt_writing(text, file):
+    with open("data/mkt_campaign.txt", "w") as f:
+        f.write(f"{text} {file} \n")
+    pass
+
+
+def array_of_client(array, cliente):
+    return list(filter(lambda x: x["name"] == cliente, array))
+
+
+def analyse_log(path):
+    with open(path) as csvfile:
+        fieldnames = ["name", "lunch", "weekday"]
+        reader = DictReader(csvfile, fieldnames=fieldnames)
+        all_clients = [row for row in reader]
+
+        maria = array_of_client(all_clients, "maria")
+        arnaldo = array_of_client(all_clients, "arnaldo")
+        joao = array_of_client(all_clients, "joao")
+
+        all_lunch = list(set([row["lunch"] for row in all_clients]))
+
+        all_days = list(set([row["weekday"] for row in all_clients]))
+
+        answer = {
+            "maria": " • Qual o prato mais pedido por 'Maria'?",
+            "arnaldo": " • Quantas vezes 'Arnaldo' pediu 'hamburguer'?",
+            "joao_n": " • Quais pratos 'João' nunca pediu?",
+            "joao_d": " • Quais dias 'Joao' nunca foi na lanchonete?",
+        }
+
+        try:
+            txt_writing(
+                answer["maria"],
+                most_frequent_maria(maria),
+            )
+
+            txt_writing(
+                answer["arnaldo"],
+                cont_frequent_arnaldo_lunch(all_lunch, arnaldo, "lunch"),
+            )
+
+            txt_writing(
+                answer["joao_n"],
+                cont_frequent_joao(all_lunch, joao, "lunch"),
+            )
+
+            txt_writing(
+                answer["joao_d"],
+                cont_frequent_joao(all_days, joao, "weekday"),
+            )
+            print("Dados gravado com sucesso")
+        except ValueError:
+            raise
+
+
+analyse_log("data/orders_1.csv")
--- tests/test_analyse_log.py
+++ tests/test_analyse_log.py
@@ -1,2 +1,29 @@
+from analyse_log import analyse_log
+
 # Crie uma suíte de testes para o método analyse_log
 # Obtenha, no mínimo, 90% de cobertura
+
+
+class Test_Analyse_Log:
+    def __init__(self):
+        self.path_orders_1 = "data/orders_1.csv"
+        self.path_orders_2 = "data/orders_2.csv"
+        self.path_mkt_campaign = "data/mkt_campaign.txt"
+
+    def __text_file_reader(self):
+        txt_file = open(self.path_mkt_campaign, "r")
+        return txt_file.read()
+
+    def test_What_is_the_most_requested_dish_for_Maria(self):
+        given = analyse_log(self.path_orders_1)
+        expected = True
+        assert given is expected
+
+    def test_How_many_times_did_Arnaldo_order_hamburger(self):
+        pass
+
+    def test_Which_dishes_João_never_ordered(self):
+        NotImplemented
+
+    def test_What_days_has_João_never_been_to_the_cafeteria(self):
+        NotImplemented
