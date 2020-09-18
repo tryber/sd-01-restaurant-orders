2ac365d1bd5e6e703aaf6c1c7a8f526cf9da4a73
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2020-09-18 18:22:22 -0300

    ajustando requisito 1, alterando e finalizando requisito 3

--- src/main.py
+++ src/main.py
@@ -1,34 +1,47 @@
 import csv
-from pubsub import pub
-from stock_control import StockControl
+# from pubsub import pub
+from inventory_control import InventoryControl
 from track_orders import TrackOrders
 
 
-def print_info(tracker, control):
-    print(tracker.get_most_ordered_dish_per_costumer('maria'))
-    print(tracker.get_order_frequency_per_costumer('arnaldo', 'hamburguer'))
-    print(tracker.get_never_ordered_per_costumer('joao'))
-    print(tracker.get_days_never_visited_per_costumer('joao'))
-    print(control.get_quantities_to_buy())
+def print_info(tracker, control, filter_data):
+    print(tracker.get_most_ordered_dish_per_costumer('maria', filter_data('maria')))
+    print(tracker.get_order_frequency_per_costumer('arnaldo', 'hamburguer', filter_data('arnaldo')))
+    print(tracker.get_never_ordered_per_costumer(filter_data('joao')))
+    print(tracker.get_days_most_visited_per_costumer(filter_data('joao')))
+    print(tracker.get_days_never_visited_per_costumer(filter_data('joao')))
+    # print(control.get_quantities_to_buy())
 
 
 def main():
-    topic = 'order'
-    path = ""
+    # topic = 'order'
+    path = "../data/orders_1.csv"
 
     tracker = TrackOrders()
-    control = StockControl()
-    subs = [tracker.add_new_order, control.add_new_order]
+    control = InventoryControl()
 
-    for sub in subs:
-        pub.subscribe(sub, topic)
+    def filter_data(customer):
+        with open(path) as csv_file:
+            filter_person = []
+            csv_reader = csv.reader(csv_file, delimiter=',')
 
-    with open(path) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        for costumer, order, day in csv_reader:
-            pub.sendMessage(topic, costumer=costumer, order=order, day=day)
+            for row in csv_reader:
+                if row[0] == customer:
+                    filter_person.append(row)
 
-    print_info(tracker, control)
+        return filter_person
+
+    # subs = [tracker.add_new_order, control.add_new_order]
+
+    # for sub in subs:
+    #     pub.subscribe(sub, topic)
+
+    # with open(path) as csv_file:
+    #     csv_reader = csv.reader(csv_file, delimiter=',')
+    #     for costumer, order, day in csv_reader:
+    #         pub.sendMessage(topic, costumer=costumer, order=order, day=day)
+
+    print_info(tracker, control, filter_data)
 
 
 if __name__ == "__main__":
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,15 +1,62 @@
 class TrackOrders:
-    def add_new_order(self, costumer, order, day):
-        pass
+    # def add_new_order(self, costumer, order, day):
+    #     pass
 
-    def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+    def get_most_ordered_dish_per_costumer(self, costumer, filter_data):
+        most_frequent_food = filter_data[0][1]
+        frequency = {}
 
-    def get_order_frequency_per_costumer(self, costumer, order):
-        pass
+        for food in filter_data:
 
-    def get_never_ordered_per_costumer(self, costumer):
-        pass
+            index_food = food[1]
 
-    def get_days_never_visited_per_costumer(self, costumer):
-        pass
+            if index_food not in frequency:
+                frequency[index_food] = 1
+
+            else:
+                frequency[index_food] += 1
+
+            if frequency[index_food] > frequency[most_frequent_food]:
+                most_frequent_food = index_food
+
+        return f"Prato favorito do(a) cliente {costumer}: {most_frequent_food}"
+
+    def get_order_frequency_per_costumer(self, costumer, order, filter_data):
+        frequency = {'hamburguer': 0, 'pizza': 0, 'coxinha': 0, 'misto-quente': 0}
+
+        for food in filter_data:
+            index_food = food[1]
+            frequency[index_food] += 1
+
+        return f"Pedido mais frequente do(a) cliente {costumer}: {frequency[order]}"
+
+    def get_never_ordered_per_costumer(self, filter_data):
+        frequency = set()
+        products = set({'hamburguer', 'pizza', 'coxinha', 'misto-quente'})
+
+        for food in filter_data:
+            frequency.add(food[1])
+
+        return f"Pedido nunca pedido pelo(a) cliente {filter_data[0][0]}: {products.difference(frequency)}"
+
+    def get_days_most_visited_per_costumer(self, filter_data):
+        present_days = []
+        days = ['segunda-feira', 'terça-feira', 'sabado']
+        days_set = set(days)
+
+        for food in filter_data:
+            present_days.append(food[2])
+
+        present_days_set = set(present_days)
+        return f"Dia(s) mais visitado(s) pelo(a) cliente {filter_data[0][0]}: {days_set.intersection(present_days_set)}"
+
+    def get_days_never_visited_per_costumer(self, filter_data):
+        present_days = []
+        days = ['segunda-feira', 'terça-feira', 'sabado']
+        days_set = set(days)
+
+        for food in filter_data:
+            present_days.append(food[2])
+
+        present_days_set = set(present_days)
+        return f"Dia(s) menos visitado(s) pelo(a) cliente {filter_data[0][0]}: {days_set.difference(present_days_set)}"
