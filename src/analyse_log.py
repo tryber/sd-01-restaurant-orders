d56f189da518a584e2bf64c3a9af868e1471425f
dougfunny <douglaseduardo220483@gmail.com>
2020-09-18 19:00:53 -0300

    terminando o requisito 1 e tentando refatorar para criar a swit de test

--- src/analyse_log.py
+++ src/analyse_log.py
@@ -43,60 +43,58 @@ def cont_frequent_joao(keys, client, params_map_key):
     return answer
 
 
-def txt_writing(text, file):
-    with open("data/mkt_campaign.txt", "w") as f:
+def txt_writing(text, file, path="data/mkt_campaign.txt"):
+    with open(path, "a") as f:
         f.write(f"{text} {file} \n")
-    pass
+    print("Dados gravado com sucesso")
 
 
 def array_of_client(array, cliente):
     return list(filter(lambda x: x["name"] == cliente, array))
 
 
-def analyse_log(path):
-    with open(path) as csvfile:
+def reading_the_main_file(path):
+    with open(path, "r") as csvfile:
         fieldnames = ["name", "lunch", "weekday"]
         reader = DictReader(csvfile, fieldnames=fieldnames)
-        all_clients = [row for row in reader]
+        return [row for row in reader]
 
-        maria = array_of_client(all_clients, "maria")
-        arnaldo = array_of_client(all_clients, "arnaldo")
-        joao = array_of_client(all_clients, "joao")
 
-        all_lunch = list(set([row["lunch"] for row in all_clients]))
+def analyse_log(path):
+    all_clients = reading_the_main_file(path)
 
-        all_days = list(set([row["weekday"] for row in all_clients]))
+    maria = array_of_client(all_clients, "maria")
+    arnaldo = array_of_client(all_clients, "arnaldo")
+    joao = array_of_client(all_clients, "joao")
 
-        answer = {
-            "maria": " • Qual o prato mais pedido por 'Maria'?",
-            "arnaldo": " • Quantas vezes 'Arnaldo' pediu 'hamburguer'?",
-            "joao_n": " • Quais pratos 'João' nunca pediu?",
-            "joao_d": " • Quais dias 'Joao' nunca foi na lanchonete?",
-        }
+    all_lunch = list(set([row["lunch"] for row in all_clients]))
 
-        try:
-            txt_writing(
-                answer["maria"],
-                most_frequent_maria(maria),
-            )
+    all_days = list(set([row["weekday"] for row in all_clients]))
 
-            txt_writing(
-                answer["arnaldo"],
-                cont_frequent_arnaldo_lunch(all_lunch, arnaldo, "lunch"),
-            )
+    answer = {
+        "maria": " • Qual o prato mais pedido por 'Maria'?",
+        "arnaldo": " • Quantas vezes 'Arnaldo' pediu 'hamburguer'?",
+        "joao_n": " • Quais pratos 'João' nunca pediu?",
+        "joao_d": " • Quais dias 'Joao' nunca foi na lanchonete?",
+    }
 
-            txt_writing(
-                answer["joao_n"],
-                cont_frequent_joao(all_lunch, joao, "lunch"),
-            )
+    try:
+        txt_writing(
+            answer["maria"],
+            most_frequent_maria(maria),
+        )
 
-            txt_writing(
-                answer["joao_d"],
-                cont_frequent_joao(all_days, joao, "weekday"),
-            )
-            print("Dados gravado com sucesso")
-        except ValueError:
-            raise
+        txt_writing(
+            answer["arnaldo"],
+            cont_frequent_arnaldo_lunch(all_lunch, arnaldo, "lunch"),
+        )
+
+        txt_writing(answer["joao_n"], cont_frequent_joao(all_lunch, joao, "lunch"))
+
+        txt_writing(answer["joao_d"], cont_frequent_joao(all_days, joao, "weekday"))
+
+    except ValueError:
+        raise
 
 
 analyse_log("data/orders_1.csv")
--- tests/test_analyse_log.py
+++ tests/test_analyse_log.py
@@ -1,29 +1,53 @@
-from analyse_log import analyse_log
+from unittest.mock import patch, mock_open
+from unittest import TestCase
+from src.analyse_log import analyse_log
 
-# Crie uma suíte de testes para o método analyse_log
-# Obtenha, no mínimo, 90% de cobertura
 
+class Test_Analyse_Log(TestCase):
 
-class Test_Analyse_Log:
-    def __init__(self):
-        self.path_orders_1 = "data/orders_1.csv"
-        self.path_orders_2 = "data/orders_2.csv"
-        self.path_mkt_campaign = "data/mkt_campaign.txt"
+    file_content_mock = """maria,hamburguer,terça-feira
+joao,hamburguer,terça-feira
+maria,pizza,terça-feira
+maria,coxinha,segunda-feira
+arnaldo,misto-quente,terça-feira
+jose,hamburguer,sabado
+maria,hamburguer,terça-feira
+maria,hamburguer,terça-feira
+joao,hamburguer,terça-feira
+maria,pizza,terça-feira
+maria,coxinha,segunda-feira
+arnaldo,misto-quente,terça-feira"""
 
-    def __text_file_reader(self):
-        txt_file = open(self.path_mkt_campaign, "r")
-        return txt_file.read()
+    fake_file_path = "file/path/mock"
 
-    def test_What_is_the_most_requested_dish_for_Maria(self):
-        given = analyse_log(self.path_orders_1)
-        expected = True
-        assert given is expected
+    def test_analyse_log(self):
+        with patch(
+            "builtins.open", mock_open(read_data=self.file_content_mock)
+        ) as mock_file:
+            assert open(self.fake_file_path).read() == self.file_content_mock
+            mock_file.assert_called_with(self.fake_file_path)
 
-    def test_How_many_times_did_Arnaldo_order_hamburger(self):
-        pass
+        expected = "sei lá!"
+        self.assertEqual(expected, actual)
 
-    def test_Which_dishes_João_never_ordered(self):
-        NotImplemented
+    # def __text_file_reader(self):
+    #     with patch('builtins.open', mock_open(read_data=self.mock)) as m:
+    #         return m.read()
 
-    def test_What_days_has_João_never_been_to_the_cafeteria(self):
-        NotImplemented
+    # def test_What_is_the_most_requested_dish_for_Maria(self):
+
+    #     with patch("builtins.open", mock_open(read_data=self.mock)) as m:
+    #         analyse_log("dummy", "dummy")
+    #         given = m.reader()
+    #         print(given)
+    #         expected = True
+    #         assert given == expected
+
+    # def test_How_many_times_did_Arnaldo_order_hamburger(self):
+    #     pass
+
+    # def test_Which_dishes_João_never_ordered(self):
+    #     NotImplemented
+
+    # def test_What_days_has_João_never_been_to_the_cafeteria(self):
+    #     NotImplemented
