86bbf37d0039acada4f1c20350e35f0417444107
vrramosm <vf.ramos7@gmail.com>
2020-09-28 10:53:04 -0300

    Refatorando todo o projeto

--- src/analyse_log.py
+++ src/analyse_log.py
@@ -1,2 +1,17 @@
+import csv
+
+
+def read_file_csv(file, plate_favorite):
+    with open(file) as csv_file:
+        filter_person = []
+        csv_reader = csv.reader(csv_file)
+
+        for row in csv_reader:
+            if row[0] == plate_favorite:
+                filter_person.append(row)
+
+    return filter_person
+
+
 def analyse_log(path_to_file):
     raise NotImplementedError
